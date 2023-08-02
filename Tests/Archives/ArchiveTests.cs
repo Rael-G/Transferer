b1b04524373dce82f8fe6730623e7ad573fadfa3
Israel Albino Galvan <israel.galvan@live.com>
2023-08-01 21:07:19 -0300

    Created first test of Archive

---
+++ Tests/Archives/ArchiveTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Api.Models;
+
+namespace Tests.Archives
+{
+    public class ArchiveTests
+    {
+        [Fact]
+        public void MustCreateArchive() 
+        {
+            string name = "arquivo.txt";
+            string type = "txt";
+            long length = 16;
+            string path = ".\\documents\\arquivo.txt";
+
+            Archive archive = new(name, type, length, path);
+
+            Assert.Equal(name, archive.FileName);
+            Assert.Equal(type, archive.ContentType);
+            Assert.Equal(length, archive.Length);
+            Assert.Equal(path, archive.Path);
+        }
+    }
+}
