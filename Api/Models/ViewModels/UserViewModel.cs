8bff3ecbaabb971c5b2125aa43de5809364a59e2
Israel Albino Galvan <israel.galvan@live.com>
2023-10-25 20:52:21 -0300

    Id is immutable

--- Api/Models/ViewModels/UserViewModel.cs
+++ Api/Models/ViewModels/UserViewModel.cs
@@ -25,7 +25,6 @@
 
         public static User MapToUser(User user, UserViewModel userViewModel)
         {
-            user.Id = userViewModel.Id;
             user.UserName = userViewModel.UserName;
 
             return user;
