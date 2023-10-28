f6aa5ee020d1babe2f4aea770eb3d075f44ce907
Israel Albino Galvan <israel.galvan@live.com>
2023-10-27 22:39:25 -0300

    Better name for AuthController

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Api.Models.InputModel;
+using Api.Business;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthBusiness _business;
+        public AuthController(IAuthBusiness business)
+        {
+            _business = business;
+        }
+
+        [HttpPost()]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> LogIn(LogInUser logInUser)
+        {
+            var loggedUser = await _business.LoginAsync(logInUser);
+
+            if (loggedUser == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            if (loggedUser.Token == null)
+            {
+                return BadRequest("Incorrect password.");
+            }
+
+            return Ok(loggedUser);
+        }
+
+        [HttpPost()]
+        [Route("signin")]
+        [AllowAnonymous]
+        public async Task<IActionResult> SignIn(LogInUser logInUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _business.CreateAsync(logInUser);
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                return BadRequest(result);
+            }
+
+            var loggedUser = await _business.LoginAsync(logInUser);
+
+            return Ok(loggedUser);
+        }
+    }
+}
