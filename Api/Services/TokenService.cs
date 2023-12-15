9907fbcd5015711abfcf12a24995310a431e775f
Israel Albino Galvan <israel.galvan@live.com>
2023-12-15 18:05:44 -0300

    Implemented refresh token and refactored gererateToken

--- Api/Services/TokenService.cs
+++ Api/Services/TokenService.cs
@@ -1,5 +1,4 @@
 ﻿using Api.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -7,18 +6,82 @@ using System.Text;
 
 namespace Api.Services
 {
-    public class TokenService
+    public static class TokenService
     {
+        public const int MinutesToExpiry = 30;
+
+        public const int DaysToExpiry = 7;
+
         /// <summary>
         /// Generates a JWT token for the specified user and user roles.
         /// </summary>
         /// <param name="user">The user for whom the token is generated.</param>
         /// <param name="userRoles">The roles associated with the user.</param>
         /// <returns>The generated JWT token as a string.</returns>
-        public static string GenerateToken(User user, List<string> userRoles)
+        public static string GenerateAccessToken(User user, List<string> userRoles)
+        {
+            var claimsIdentity = GenerateClaimsIdentity(user, userRoles);
+
+            return GenerateAccessToken(claimsIdentity);
+        }
+
+        /// <summary>
+        /// Generates a JWT token based on the provided claims.
+        /// </summary>
+        /// <param name="claims">The claims to include in the token.</param>
+        /// <returns>The generated JWT token as a string.</returns>
+        public static string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var claimsIdentity = GenerateClaimsIdentity(claims);
+
+            return GenerateAccessToken(claimsIdentity);
+        }
+
+        /// <summary>
+        /// Generates a refresh token using a new Guid.
+        /// </summary>
+        /// <returns>The generated refresh token as a string.</returns>
+        public static string GenerateRefreshToken()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// Retrieves a ClaimsPrincipal from an expired JWT token.
+        /// </summary>
+        /// <param name="token">The expired JWT token.</param>
+        /// <returns>The ClaimsPrincipal extracted from the token.</returns>
+        public static ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var key = Encoding.ASCII.GetBytes(Settings.SecretKey);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = false
+            };
+            var principal = new JwtSecurityTokenHandler()
+                .ValidateToken(token, tokenValidationParameters, out var securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg.Equals
+                (SecurityAlgorithms.HmacSha256, StringComparison.InvariantCulture))
+                throw new SecurityTokenException("Invalid Token.");
+            
+            return principal;
+        }
+
+        /// <summary>
+        /// Generates a JWT token based on the provided claims identity.
+        /// </summary>
+        /// <param name="claimsIdentity">The claims identity for the token.</param>
+        /// <returns>The generated JWT token as a string.</returns>
+        private static string GenerateAccessToken(ClaimsIdentity claimsIdentity)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(Settings._secret);
+            var key = Encoding.ASCII.GetBytes(Settings.SecretKey);
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
@@ -26,9 +89,9 @@ namespace Api.Services
                     new SymmetricSecurityKey(key),
                     SecurityAlgorithms.HmacSha256Signature),
 
-                Expires = DateTime.UtcNow.AddDays(1),
+                Expires = DateTime.UtcNow.AddMinutes(MinutesToExpiry),
 
-                Subject = GenerateClaims(user, userRoles)
+                Subject = claimsIdentity
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
@@ -41,9 +104,9 @@ namespace Api.Services
         /// <param name="user">The user for whom the claims are generated.</param>
         /// <param name="userRoles">The roles associated with the user.</param>
         /// <returns>A ClaimsIdentity containing user-specific and role claims.</returns>
-        private static ClaimsIdentity GenerateClaims(User user, List<string> userRoles) 
+        private static ClaimsIdentity GenerateClaimsIdentity(User user, List<string> userRoles) 
         {
-            var claims = new ClaimsIdentity(new[]
+            var claimsIdentity = new ClaimsIdentity(new[]
                 {
                     new Claim("UserId", user.Id),
                     new Claim(ClaimTypes.Name, user.UserName)
@@ -51,10 +114,22 @@ namespace Api.Services
 
             foreach(var role in userRoles)
             {
-                claims.AddClaim(new Claim(ClaimTypes.Role, role));
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
             }
 
-            return claims;
+            return claimsIdentity;
+        }
+
+        /// <summary>
+        /// Generates a ClaimsIdentity based on the provided claims.
+        /// </summary>
+        /// <param name="claims">The claims to include in the identity.</param>
+        /// <returns>A ClaimsIdentity containing the provided claims.</returns>
+        private static ClaimsIdentity GenerateClaimsIdentity(IEnumerable<Claim> claims)
+        {
+            var claimsIdentity = new ClaimsIdentity(claims);
+
+            return claimsIdentity;
         }
     }
 }
