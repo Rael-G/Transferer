5a67c4cbdf8ba2b7e1c2aa81ebc3ad2cda035eff
Israel Albino Galvan <israel.galvan@live.com>
2023-12-15 18:02:05 -0300

    Better View model for login

--- Api/Models/ViewModels/LoggedUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Api.Models.ViewModels
-{
-    public record LoggedUser
-    {
-        public string UserName { get; set; }
-        public string Token { get; set; }
-    }
-}
---
+++ Api/Models/ViewModels/TokenViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models.ViewModels
+{
+    public record TokenViewModel
+    {
+        public string AccessToken { get; set; }
+        public DateTime Creation { get; set; }
+        public DateTime Expiration { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
